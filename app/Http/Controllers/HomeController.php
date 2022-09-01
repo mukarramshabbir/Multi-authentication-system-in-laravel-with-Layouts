5096fac124ad02466ac8383441a53f939854a195
Mukarram Ali <mukarramali623@gmail.com>
2022-09-01 11:38:44 -0700

    First Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function logout(){
+        //Auth::logout();
+        Auth::logout();
+        $notification=array(
+            'message'=>'Successfully Logout',
+            'alert-type'=>'success'
+        );
+        return Redirect()->route('login')->with($notification);
+    }
+}
